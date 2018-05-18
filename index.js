f6923dcfab4a898573dd8a95fe0bc54cf78bba6d
Sara Ledger <sara.ledger@live.com>
2018-05-18 14:18:30 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,32 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  object['key'] = value
+  return object
+}
+
+//come back to
+function updateObjectWithKeyAndValue(object, key, value) {
+  object['key'] = value
+  return object
+}
+
+function destructivelyUpdateObjectWithKeyAndvalue(object, key, value) {
+
+}
+
+function deleteFromObjectByKey(object, key) {
+
+}
+
+function deleteFromObjectByKey(object, key) {
+
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+
+}
